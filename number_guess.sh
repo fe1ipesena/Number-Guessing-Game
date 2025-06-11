85575a16c76770ecb3bf725912206183f46795bc
Felipe Carvalho Sena <felipecsena877@gmail.com>
2025-06-11 01:50:17 +0000

    feat: save user to db

--- number_guess.sh
+++ number_guess.sh
@@ -6,6 +6,20 @@ echo $RANDOM_NUMBER
 echo "Enter your username:"
 read USERNAME
 
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+#saving the user
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+if [[ -z $USER_ID ]]; then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  $PSQL "INSERT INTO users(username) VALUES('$USERNAME')"
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+else
+  GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID")
+  BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USER_ID")
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
+
 echo "Guess the secret number between 1 and 1000:"
 while true; do
   read GUESS
@@ -14,6 +28,7 @@ while true; do
     echo "That is not an integer, guess again:"
   elif [[ $GUESS -eq $RANDOM_NUMBER ]]; then
     echo "You guessed it in $TRIES tries. The secret number was $RANDOM_NUMBER. Nice job!"
+    #$PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, $TRIES)"
     break
   elif [[ $GUESS -gt $RANDOM_NUMBER ]]; then
     echo "It's lower than that, guess again:"
