e6901022e10927f1de94ca8cd057fd37d4c9b224
Felipe Carvalho Sena <felipecsena877@gmail.com>
2025-06-11 01:47:05 +0000

    feat: number guessing

--- number_guess.sh
+++ number_guess.sh
@@ -5,3 +5,19 @@ echo $RANDOM_NUMBER
 
 echo "Enter your username:"
 read USERNAME
+
+echo "Guess the secret number between 1 and 1000:"
+while true; do
+  read GUESS
+  ((TRIES++))
+  if ! [[ $GUESS =~ ^[0-9]+$ ]]; then
+    echo "That is not an integer, guess again:"
+  elif [[ $GUESS -eq $RANDOM_NUMBER ]]; then
+    echo "You guessed it in $TRIES tries. The secret number was $RANDOM_NUMBER. Nice job!"
+    break
+  elif [[ $GUESS -gt $RANDOM_NUMBER ]]; then
+    echo "It's lower than that, guess again:"
+  else
+    echo "It's higher than that, guess again:"
+  fi
+done
